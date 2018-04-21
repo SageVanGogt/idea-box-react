dfcdd9ad8a417848a8f55a7c96122f4ca1e306e8
Sage Vogt <thurmanvogt@gmail.com>
2018-04-21 14:32:40 -0600

    Add ideaform component bare bones and now rendering

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './styles/IdeaForm.css';
 
 const IdeaForm = () => {
-
+  return ( 
+    <form action="">
+      <input type="text" placeholder="name" />
+      <input type="text" name="" id="" placeholder="idea" />
+      <input type="submit"/>
+    </form>
+  )
 }
 
 export default IdeaForm;
--- src/components/IdeaList.js
+++ src/components/IdeaList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/IdeaList.css';
 
 const IdeaList = () => {
 
